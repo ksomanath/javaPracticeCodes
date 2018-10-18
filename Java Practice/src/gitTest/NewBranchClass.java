9d8e8b98c83084e30bee7c1f8657338b26a795ba
somanath-kumbhar <somanathkumbhar04@gmail.com>
2018-10-18 11:19:07 +0530

    New Branch updates

--- Java Practice/src/gitTest/NewBranchClass.java
+++ Java Practice/src/gitTest/NewBranchClass.java
@@ -4,5 +4,8 @@ public class NewBranchClass {
 
 	public static void main(String[] args) {
 		System.out.println("===============================");
+		System.out.println("===============================");
+		System.out.println("===============================");
+		System.out.println("===============================");
 	}
 }
